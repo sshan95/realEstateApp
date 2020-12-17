1268a7354e5ba8fcd1e6f63153f7c901e0c86466
Samuel Han <realsamuelhan95@gmail.com>
2020-12-17 15:30:31 -0500

    added local_settings

--- realEstateApp/settings.py
+++ realEstateApp/settings.py
@@ -149,3 +149,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'realsamuelhan95@gmail.com'
 EMAIL_HOST_PASSWORD = 'email'
 EMAIL_USE_TLS=True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
